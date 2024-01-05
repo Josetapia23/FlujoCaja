d9863a2d03ffef531afd484cb7cbc801d0715b29
Orla08 <91040882+Orla08@users.noreply.github.com>
2024-01-05 16:11:02 -0500

    Agragando splashScreens

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View , StatusBar} from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View , StatusBar, Platform} from 'react-native'
+import React, { useEffect } from 'react'
 import Navegacion from './src/componentes/Navegacion'
 import { AuthProvider } from './src/context/AuthContext'
+import SplashScreen from 'react-native-splash-screen';
 
 
-StatusBar.setBackgroundColor('#69C2E3'); // Color de la barra de estado
+StatusBar.setBackgroundColor('#003240'); // Color de la barra de estado
 
 const App = () => {
+
+  useEffect(()=>{
+    if(Platform.OS === 'android') SplashScreen.hide();
+  },[])
 
   return (
     <AuthProvider>
--- android/app/src/main/java/com/manejocaja/MainActivity.java
+++ android/app/src/main/java/com/manejocaja/MainActivity.java
@@ -1,13 +1,19 @@
 package com.manejocaja;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -30,11 +36,4 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
-
-  // ...
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
-  // ...
 }
